d2e8a135812ab4329715b0dea3605bb9435e51d1
김기범 <gibeom@gimgibeoms-MacBook-Air.local>
2023-04-01 18:39:46 +0900

    add project history to the Profile Detail page

--- lib/profile_detail_page.dart
+++ lib/profile_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:portfolio/project.dart';
 import 'utility.dart';
 
 class ProfileDetailPage extends StatelessWidget {
@@ -22,10 +24,7 @@ class ProfileDetailPage extends StatelessWidget {
 
   Widget buildNarrowLayout(BuildContext context) {
     double paddingValue = 12;
-    final double screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
+    final double screenWidth = MediaQuery.of(context).size.width;
 
     paddingValue = getPaddingValue(screenWidth);
 
@@ -33,11 +32,10 @@ class ProfileDetailPage extends StatelessWidget {
       padding: EdgeInsets.only(
           top: 12, bottom: 12, left: paddingValue, right: paddingValue),
       child: FutureBuilder<DocumentSnapshot>(
-        future: FirebaseFirestore.instance.collection('users')
-            .doc('profile')
-            .get(),
-        builder: (BuildContext context,
-            AsyncSnapshot<DocumentSnapshot> snapshot) {
+        future:
+            FirebaseFirestore.instance.collection('users').doc('profile').get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.hasError) {
             return const Center(
               child: Text('에러가 발생했습니다.'),
@@ -51,7 +49,7 @@ class ProfileDetailPage extends StatelessWidget {
           final data = snapshot.data!.data() as Map<String, dynamic>;
 
           return ListView.builder(
-            itemCount: 6,
+            itemCount: 7,
             itemBuilder: (BuildContext context, int index) {
               switch (index) {
                 case 0:
@@ -71,6 +69,8 @@ class ProfileDetailPage extends StatelessWidget {
                   return buildExperience(context, data);
                 case 5:
                   return buildLabExperience(context, data);
+                case 6:
+                  return buildProjectList(context, data);
                 default:
                   return const SizedBox.shrink();
               }
@@ -99,14 +99,8 @@ class ProfileDetailPage extends StatelessWidget {
               ),
               Center(
                 child: SizedBox(
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height * 0.7,
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  width: MediaQuery.of(context).size.width * 0.8,
                   child: PhotoView(
                     imageProvider: NetworkImage(
                       imageUrl,
@@ -212,10 +206,7 @@ class ProfileDetailPage extends StatelessWidget {
           const SizedBox(height: 8),
           Text(
             data['introduction'],
-            style: Theme
-                .of(context)
-                .textTheme
-                .titleMedium,
+            style: Theme.of(context).textTheme.titleMedium,
           ),
         ],
       ),
@@ -251,11 +242,13 @@ class ProfileDetailPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           buildSubtitle('경력'),
-          const SizedBox(height: 8),
+          const SizedBox(height: 12),
           for (String experience in experiences) ...[
             Row(
               children: [
-                SizedBox(width: 10,),
+                SizedBox(
+                  width: 10,
+                ),
                 Text(
                   "• ",
                   style: Theme.of(context).textTheme.titleMedium,
@@ -268,13 +261,12 @@ class ProfileDetailPage extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 4),
+            const SizedBox(height: 8),
           ],
         ],
       ),
     );
   }
-
 
   Widget buildLabExperience(BuildContext context, Map<String, dynamic> data) {
     List<String> experiences = List<String>.from(data['조교활동']);
@@ -284,11 +276,13 @@ class ProfileDetailPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           buildSubtitle('조교활동'),
-          const SizedBox(height: 8),
+          const SizedBox(height: 12),
           for (String experience in experiences) ...[
             Row(
               children: [
-                SizedBox(width: 10,),
+                SizedBox(
+                  width: 10,
+                ),
                 Text(
                   "• ",
                   style: Theme.of(context).textTheme.titleMedium,
@@ -301,13 +295,12 @@ class ProfileDetailPage extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 4),
+            const SizedBox(height: 8),
           ],
         ],
       ),
     );
   }
-
 
   Widget buildAwardList(BuildContext context, Map<String, dynamic> data) {
     List<String> experiences = List<String>.from(data['수상내역']);
@@ -317,11 +310,13 @@ class ProfileDetailPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           buildSubtitle('주요성과'),
-          const SizedBox(height: 8),
+          const SizedBox(height: 12),
           for (String experience in experiences) ...[
             Row(
               children: [
-                SizedBox(width: 10,),
+                SizedBox(
+                  width: 10,
+                ),
                 Text(
                   "• ",
                   style: Theme.of(context).textTheme.titleMedium,
@@ -334,7 +329,7 @@ class ProfileDetailPage extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 4),
+            const SizedBox(height: 8),
           ],
         ],
       ),
@@ -342,13 +337,132 @@ class ProfileDetailPage extends StatelessWidget {
   }
 
   Text buildSubtitle(String string) {
-    return  Text(
-        string,
+    return Text(string,
         style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.bold,
-        )
-    );
+        ));
+  }
 
+  String getProjectDurationString(Project project) {
+    final startYear = project.startDate.year;
+    final endYear = project.endDate.year;
+    final startMonth = project.startDate.month;
+    final endMonth = project.endDate.month;
+
+    if (startYear == endYear) {
+      if (startMonth == endMonth) {
+        return '$startYear년 $startMonth월';
+      } else {
+        return '$startYear년 $startMonth월 ~ $endMonth월';
+      }
+    } else {
+      return '$startYear년 $startMonth월 ~ $endYear년 $endMonth월';
+    }
+  }
+
+  Widget buildProjectList(BuildContext context, Map<String, dynamic> data) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildSubtitle("프로젝트 연혁"),
+          SizedBox(height: 8),
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('Projects')
+                .orderBy('startDate', descending: false)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('에러가 발생했습니다.'),
+                );
+              }
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final projects = snapshot.data!.docs.map((doc) {
+                final data = doc.data() as Map<String, dynamic>;
+                final List<String> imageUrls =
+                    (data['imageUrls'] as List<dynamic>)
+                        .map((url) => url as String)
+                        .toList();
+                final List<String> skills = (data['주요기술'] as List<dynamic>)
+                    .map((skill) => skill as String)
+                    .toList();
+                final bool isCurrent = data['endDate'] == null;
+
+                return Project(
+                    name: data['projectName'] as String,
+                    description: data['description'] as String,
+                    imageUrl: data['imageUrl'] as String,
+                    startDate: (data['startDate'] as Timestamp).toDate(),
+                    endDate: isCurrent
+                        ? DateTime.now()
+                        : (data['endDate'] as Timestamp).toDate(),
+                    imageUrls: imageUrls,
+                    skills: skills,
+                    importance: data['중요도'] as String,
+                    category: data['category'] as String,
+                    impression: data['느낀점'] as String);
+              }).toList();
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: projects
+                    .map(
+                      (project) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "• ",
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Text(
+                                '${DateFormat('yyyy.MM').format(project.startDate)} ~ ${DateFormat('yyyy.MM').format(project.endDate)}: ',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Expanded(
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/projectDetail', arguments: project);
+                                  },
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      '[${project.category}] ${project.name}',
+                                      style: TextStyle(
+                                        color: Colors.blueAccent,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          //const SizedBox(height: 4),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
